fade1cb57d1d13c88d53419c6a2d4d2e4f9bdd1e
evan <evanfauzansamiswara@gmail.com>
2024-10-15 15:25:33 +0700

    rombak

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('nav ul');
+
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        navList.style.animation = navList.classList.contains('active') ? 'fadeInMenu 0.3s ease forwards' : 'none';
+    });
+
+
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slides img');
 const totalSlides = slides.length;
@@ -13,3 +23,14 @@ function nextSlide() {
 
 // Set interval for automatic photo show
 setInterval(nextSlide, 3000);
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert("Semua field harus diisi!");
+        return false; // Mencegah pengiriman form
+    }
+    return true; // Mengizinkan pengiriman form
+}
